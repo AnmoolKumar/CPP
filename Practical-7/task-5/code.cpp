2ab2861aafea104bc518cae4b59e53307142b900
Achintya Mishra <76170002+axhaar@users.noreply.github.com>
2021-10-17 20:18:48 +0530

    Update code.cpp

--- Practical-7/task-5/code.cpp
+++ Practical-7/task-5/code.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 int main()
 {
-    int m[4][4];
+    char arr[4][4];
     for(int i=0;i<4;i++)
     {
         for(int j=0;j<=i;j++)
         {
-            cout<<"*"<<" ";
+            arr[i][j] = '*';
+        }
+    }
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<arr[i][j]<<" ";
         }
         cout<<"\n";
     }
